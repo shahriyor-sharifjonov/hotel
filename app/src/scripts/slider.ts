ac74a7763b8e041f1e7be0459cb3f51667c57d48
Shahriyor <shahriyordeveloping@gmail.com>
2021-07-17 00:34:01 +0300

    intial

--- app/src/scripts/slider.ts
+++ app/src/scripts/slider.ts
@@ -29,3 +29,65 @@ var swiper = new Swiper(".mySwiper", {
     }
   }
 });
+var swiper = new Swiper(".mySwiper2", {
+  grabCursor: true,
+  centeredSlides: false,
+  slidesPerView: "3",
+  initialSlide: 1,
+  speed: 400,
+  spaceBetween: 50,
+  pagination: {
+    el: ".swiper-pagination2",
+    clickable:true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    530: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      centeredSlides: false,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 50
+    }
+  }
+});
+var swiper = new Swiper(".mySwiper3", {
+  grabCursor: true,
+  centeredSlides: false,
+  slidesPerView: "3",
+  initialSlide: 1,
+  speed: 400,
+  spaceBetween: 50,
+  pagination: {
+    el: ".swiper-pagination3",
+    clickable:true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    530: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      centeredSlides: false,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 50
+    }
+  }
+});
